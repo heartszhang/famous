d9823d34e969a0641fed2b1bb2a383c3ee1a196a
hearts <hearts.zhang@gmail.com>
2013-11-05 23:28:12 +0800

    - add function CleanFragment
    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- src/github.com/heartszhang/cleaner/extract.go
+++ src/github.com/heartszhang/cleaner/extract.go
@@ -2,13 +2,27 @@ package cleaner
 
 import (
 	"code.google.com/p/go.net/html"
+	"io/ioutil"
 )
 
 func MakeFragmentReadable(doc *html.Node) (*html.Node, *DocSummary, error) {
 	article := html_clean_fragment(doc)
+	//	of, err := write_file(doc)
+
 	doc1, article := readabilitier_make_readable(article)
+	//	of, err = write_file(doc1)
+
 	article = boiler_clean_by_link_density(article)
+	//	of, err = write_file(doc1)
+
 	article = boiler_clean_form_prefix(article)
+	//	of, err = write_file(doc1)
+	return article, new_docsummary(doc1), nil
+}
+
+func CleanFragment(doc *html.Node) (*html.Node, *DocSummary, error) {
+	article := html_clean_fragment(doc)
+	doc1, article := readabilitier_make_readable(article)
 	return article, new_docsummary(doc1), nil
 }
 
@@ -27,4 +41,15 @@ func MakeHtmlReadable(doc *html.Node, url string) (*html.Node, *DocSummary, erro
 	article = boiler_clean_form_prefix(article)
 	//	h5ml, err := WriteHtmlFile2(doc1)
 	return article, new_docsummary(doc1), nil
+}
+
+func write_file(doc *html.Node) (string, error) {
+	of, err := ioutil.TempFile("", "html.")
+	if err != nil {
+		return "", err
+	}
+	defer of.Close()
+
+	html.Render(of, doc)
+	return of.Name(), nil
 }
