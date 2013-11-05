6eda9650cab2979dd36e392d41239669a77219cd
hearts <hearts.zhang@gmail.com>
2013-11-05 23:38:47 +0800

    Signed-off-by: hearts <hearts.zhang@gmail.com>

---
+++ famousfront/controls/FlowTextBlock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Markup;
+using System.Xml;
+
+namespace famousfront.controls
+{
+    class FlowTextBlock : RichTextBox
+    {
+        public static readonly DependencyProperty DocumentProperty =
+            DependencyProperty.Register("Document", typeof(string),
+            typeof(FlowTextBlock), new FrameworkPropertyMetadata
+            (null, new PropertyChangedCallback(OnDocumentChanged)));
+
+        public new string Document
+        {
+            get
+            {
+                return (string)this.GetValue(DocumentProperty);
+            }
+
+            set
+            {
+                this.SetValue(DocumentProperty, value);
+            }
+        }
+
+        public static void OnDocumentChanged(DependencyObject obj,
+            DependencyPropertyChangedEventArgs args)
+        {
+            RichTextBox rtb = (RichTextBox)obj;
+            var fdoc = XamlReader.Load(new XmlTextReader(new System.IO.StringReader((string)args.NewValue))) as FlowDocument;
+            rtb.Document = fdoc;
+        }
+    }
+}
