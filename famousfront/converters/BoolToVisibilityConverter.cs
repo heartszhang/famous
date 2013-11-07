d6d18e9c6d883c06f41f807a0644a8a27df8e949
hearts <hearts.zhang@gmail.com>
2013-11-07 17:03:01 +0800

    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- famousfront/converters/BoolToVisibilityConverter.cs
+++ famousfront/converters/BoolToVisibilityConverter.cs
@@ -126,4 +126,25 @@ namespace famousfront.converters
             throw new NotImplementedException();
         }
     }
+    [ValueConversion(typeof(bool), typeof(Elysium.Controls.ProgressState))]
+    internal class BoolToBusyIndicatorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Elysium.Controls.ProgressState.Normal;
+
+            var boolean = (bool)value;
+
+            if (parameter != null)
+                boolean = !boolean;
+
+            return boolean ? Elysium.Controls.ProgressState.Indeterminate : Elysium.Controls.ProgressState.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
