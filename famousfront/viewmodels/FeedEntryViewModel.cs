de623d48fca341ce708d585aee377e7d1ba9a352
hearts <hearts.zhang@gmail.com>
2013-11-09 14:14:24 +0800

    - add image support
    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- famousfront/viewmodels/FeedEntryViewModel.cs
+++ famousfront/viewmodels/FeedEntryViewModel.cs
@@ -7,6 +7,8 @@ using famousfront.datamodels;
 using System.Windows.Documents;
 using System.Windows.Markup;
 using System.Xml;
+using famousfront.core;
+using System.Diagnostics;
 namespace famousfront.viewmodels
 {
     class FeedEntryViewModel : famousfront.core.ViewModelBase
@@ -18,6 +20,31 @@ namespace famousfront.viewmodels
         {
             _ = v;
             _pub_day = publish_day();
+            HasDocument = (_.status & FeedStatuses.Feed_status_text_many) != 0;
+            var inline = _.status & FeedStatuses.Feed_status_media_inline;
+            var imgone = _.status & FeedStatuses.Feed_status_image_one;
+            var imgmany = _.status & FeedStatuses.Feed_status_image_many;
+            var media = _.status & (FeedStatuses.Feed_status_media_one | FeedStatuses.Feed_status_media_many);
+            if (media != 0)
+            {
+                Media = new MediaElementViewModel(_.videos[0]);
+                HasMedia = true;
+            }
+            if (imgone != 0 && inline == 0) {
+                Media = new ImageElementViewModel(_.images[0]);
+                HasMedia = true;
+            }
+            else if (imgmany != 0 && inline == 0)
+            {
+                Media = new ImageGalleryViewModel(_.images);
+                HasMedia = true;
+            }
+        }
+        bool _has_media = false;
+        public bool HasMedia
+        {
+            get { return _has_media; }
+            private set { Set(ref _has_media, value); }
         }
         public string Summary { get { return _.summary; } }
         public string Title { get { return _.title.main; } }
@@ -29,6 +56,18 @@ namespace famousfront.viewmodels
         {
             var p = utime.AddMilliseconds(_.pubdate / 1000000);
             return p.ToString("D");
+        }
+        TaskViewModel _media;
+        public TaskViewModel Media
+        {
+            get { return _media; }
+            private set {Set(ref _media, value);}
+        }
+        bool _has_document = true;
+        public bool HasDocument
+        {
+            get { return _has_document; }
+            private set { Set(ref _has_document, value); }
         }
         /*
         FlowDocument _doc;
