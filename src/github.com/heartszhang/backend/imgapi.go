6b4b158418f3f3eef7b23a0260ab41a4568b190e
hearts <hearts.zhang@gmail.com>
2013-11-09 14:16:10 +0800

    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- src/github.com/heartszhang/backend/imgapi.go
+++ src/github.com/heartszhang/backend/imgapi.go
@@ -2,18 +2,11 @@ package backend
 
 import (
 	"github.com/heartszhang/curl"
+	"github.com/heartszhang/feedfeed"
 )
 
-type ImageCache struct {
-	Mime           string `json:"mime,omitempty" bson:"mime,omitempty"`
-	ThumbnailLocal string `json:"thumbnail,omitempty" bson:"thumbnail,omitempty"`
-	OriginLocal    string `json:"origin,omitempty" bson:"origin,omitempty"`
-	Code           int    `json:"code" bson:"code"`
-	Width          int    `json:"width" bson:"width"`
-	Height         int    `json:"height" bson:"height"`
-}
-
-func image_get_or_cache(uri string) (ImageCache, error) {
+// /feed/entry/image.json/{url}/{entry_id}
+func image_description(uri string) (feedfeed.FeedImage, error) {
 	imgo := new_imagecache_operator()
 	v, err := imgo.find(uri)
 
@@ -30,6 +23,6 @@ func image_get_or_cache(uri string) (ImageCache, error) {
 		return v, err
 	}
 	v.ThumbnailLocal, v.Mime, v.Width, v.Height, err = curl.NewThumbnail(cache.Local, config.ThumbnailDir, config.ThumbnailWidth, 0)
-	imgo.save(uri, v)
+	go imgo.save(uri, v)
 	return v, err
 }
