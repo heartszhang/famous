97276d1f237e645009f9eb0d66b4988aa9a2b6f1
hearts <hearts.zhang@gmail.com>
2013-11-14 08:51:56 +0800

    - add feedimageentry feedmediaentry datatemplate
    Signed-off-by: hearts <hearts.zhang@gmail.com>

---
+++ famousfront/views/FeedEntryTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using famousfront.viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace famousfront.views
+{
+    class FeedEntryTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item,    DependencyObject container)
+        {
+            var ws = item as FeedEntryViewModel;
+            if (ws == null)
+                return base.SelectTemplate(item, container);
+            var fe = container as FrameworkElement;
+            var mot = fe.FindResource("MediaOneTemplate") as DataTemplate;
+            if (ws.HasMedia && !ws.HasDocument)
+                return mot ?? base.SelectTemplate(item, container);
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
