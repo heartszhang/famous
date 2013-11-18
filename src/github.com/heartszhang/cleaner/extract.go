c0628666e1367d8444e4ffc9564e57e5012f7844
hearts <hearts.zhang@gmail.com>
2013-11-19 01:21:46 +0800

    - log clean steps
    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- src/github.com/heartszhang/cleaner/extract.go
+++ src/github.com/heartszhang/cleaner/extract.go
@@ -3,29 +3,42 @@ package cleaner
 import (
 	"code.google.com/p/go.net/html"
 	"io/ioutil"
-	//	"log"
+	"log"
 )
 
-func MakeFragmentReadable(doc *html.Node) (*html.Node, *DocumentSummary, error) {
+type Extractor interface {
+	MakeFragmentReadable(node *html.Node) (*html.Node, *DocumentSummary, error)
+	CleanFragment(doc *html.Node) (*html.Node, *DocumentSummary, error)
+	MakeHtmlReadable(doc *html.Node, url string) (*html.Node, *DocumentSummary, error)
+}
+
+type html_extractor struct {
+	temp_dir string
+}
+
+func NewExtractor(tmpdir string) Extractor {
+	return html_extractor{temp_dir: tmpdir}
+}
+func (this html_extractor) MakeFragmentReadable(doc *html.Node) (*html.Node, *DocumentSummary, error) {
 	article := html_clean_fragment(doc)
-	//	of, err := write_file(doc)
-	//	log.Println("clean-fragment", of, err)
+	of, err := write_file(doc, this.temp_dir)
+	log.Println("clean-fragment", of, err)
 
 	doc1, article := readabilitier_make_readable(article)
-	//	of, err = write_file(doc1)
-	//	log.Println("make-readable", of, err)
+	of, err = write_file(doc1, this.temp_dir)
+	log.Println("make-readable", of, err)
 
 	article = boiler_clean_by_link_density(article)
-	//	of, err = write_file(doc1)
-	//	log.Println("clean-by-density", of, err)
+	of, err = write_file(doc1, this.temp_dir)
+	log.Println("clean-by-density", of, err)
 
 	article = boiler_clean_form_prefix(article)
-	//	of, err = write_file(doc1)
-	//	log.Println("clean-form", of, err)
+	of, err = write_file(doc1, this.temp_dir)
+	log.Println("clean-form", of, err)
 	return article, new_docsummary(doc1), nil
 }
 
-func CleanFragment(doc *html.Node) (*html.Node, *DocumentSummary, error) {
+func (this html_extractor) CleanFragment(doc *html.Node) (*html.Node, *DocumentSummary, error) {
 	article := html_clean_fragment(doc)
 	doc1, article := readabilitier_make_readable(article)
 	return article, new_docsummary(doc1), nil
@@ -33,7 +46,7 @@ func CleanFragment(doc *html.Node) (*html.Node, *DocumentSummary, error) {
 
 // cleaned html
 // return filepath, *SummaryScore, error
-func MakeHtmlReadable(doc *html.Node, url string) (*html.Node, *DocumentSummary, error) {
+func (this html_extractor) MakeHtmlReadable(doc *html.Node, url string) (*html.Node, *DocumentSummary, error) {
 	article := html_clean_root(doc, url)
 
 	//	s2, _ := WriteHtmlFile2(doc)
@@ -48,8 +61,8 @@ func MakeHtmlReadable(doc *html.Node, url string) (*html.Node, *DocumentSummary,
 	return article, new_docsummary(doc1), nil
 }
 
-func write_file(doc *html.Node) (string, error) {
-	of, err := ioutil.TempFile("", "html.")
+func write_file(doc *html.Node, temp string) (string, error) {
+	of, err := ioutil.TempFile(temp, "html.")
 	if err != nil {
 		return "", err
 	}
