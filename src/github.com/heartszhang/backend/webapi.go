689e9dd88f205aeabb4e7f5a59b488a0d8d3a319
hearts <hearts.zhang@gmail.com>
2013-11-20 15:26:11 +0800

    - feature: 显示feedsource概要
    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- src/github.com/heartszhang/backend/webapi.go
+++ src/github.com/heartszhang/backend/webapi.go
@@ -43,14 +43,28 @@ func init() {
 	http.HandleFunc("/api/link/origin.json", webapi_link_origin)          // ?uri=
 	http.HandleFunc("/api/suggest/bing.json", webapi_suggest_bing)        // ?q=
 	http.HandleFunc("/api/feed_source/find.json", webapi_feedsource_find) // ?q=
+	http.HandleFunc("/api/feed_source/show.json", webapi_feedsource_find) // ?q=
 	http.HandleFunc("/exit.json", webapi_exit)
 	http.HandleFunc("/", webapi_home)
 }
 
 const uint64_bits int = 64
 
+func webapi_feedsource_show(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query().Get("q")
+	switch fs, err := feedsource_show(q); err {
+	case nil:
+		webapi_write_as_json(w, fs)
+	default:
+		webapi_write_error(w, err)
+	}
+}
 func webapi_feedsource_find(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query().Get("q")
+	if _, err := url.Parse(q); err == nil {
+		webapi_feedsource_show(w, r)
+		return
+	}
 	switch fs, err := feedsource_find(q); err {
 	case nil:
 		webapi_write_as_json(w, fs)
@@ -93,9 +107,8 @@ func webapi_feedcategory_all(w http.ResponseWriter, r *http.Request) {
 // uri: /api/feed_entry/unread.json/{uri}/{count}/{page}
 func webapi_feedentry_unread(w http.ResponseWriter, r *http.Request) {
 	//	category, err := strconv.ParseUint(r.URL.Query().Get("category"), 0, uint64_bits)
-	log.Println(r.URL.RequestURI())
-	//uri, _ := url.QueryUnescape(r.URL.Query().Get("uri"))
 	uri := r.URL.Query().Get("uri")
+	log.Println("feedentry-unread", uri)
 	count, _ := strconv.ParseUint(r.URL.Query().Get("count"), 0, 0)
 	page, _ := strconv.ParseUint(r.URL.Query().Get("page"), 0, 0)
 	switch fe, err, sc := feedentry_unread(uri, uint(count), uint(page)); err {
@@ -106,17 +119,19 @@ func webapi_feedentry_unread(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// uri: /feed_source/subscribe.json/{uri}/{source_type}/{category}
+// uri: /feed_source/subscribe.json?uri=
 func webapi_feedsource_subscribe(w http.ResponseWriter, r *http.Request) {
 	uri := r.URL.Query().Get("uri")
+	log.Println("feedsource-sub", uri)
 	source_type := source_type_map(r.URL.Query().Get("source_type"))
 	switch fs, err := feedsource_subscribe(uri, source_type); err {
 	case nil:
+		log.Println(fs, err)
 		webapi_write_as_json(w, fs)
 	default:
+		log.Println(err)
 		webapi_write_error(w, err)
 	}
-	log.Println(r.URL.RequestURI())
 }
 
 // uri: /meta.json
@@ -134,6 +149,7 @@ func webapi_meta(w http.ResponseWriter, r *http.Request) {
 // uri: /feed_entry/mark.json/{entry_id}/{flags}
 func webapi_feedentry_mark(w http.ResponseWriter, r *http.Request) {
 	uri := r.URL.Query().Get("entry_uri")
+	log.Println("feedentry-mark", uri)
 	f, err := strconv.ParseUint(r.URL.Query().Get("flags"), 0, 0)
 	flag := uint(f)
 	if err == nil {
@@ -144,13 +160,12 @@ func webapi_feedentry_mark(w http.ResponseWriter, r *http.Request) {
 	} else {
 		webapi_write_as_json(w, struct{}{})
 	}
-	log.Println(r.URL.RequestURI())
-
 }
 
 // uri: /feed_entry/umark.json/{entry_id}/{flags}
 func webapi_feedentry_umark(w http.ResponseWriter, r *http.Request) {
 	uri := r.URL.Query().Get("entry_uri")
+	log.Println("feedentry-umark", uri)
 	f, err := strconv.ParseInt(r.URL.Query().Get("flags"), 0, 0)
 	flag := uint(f)
 	if err == nil {
@@ -161,68 +176,65 @@ func webapi_feedentry_umark(w http.ResponseWriter, r *http.Request) {
 	} else {
 		webapi_write_as_json(w, struct{}{})
 	}
-	log.Println(r.URL.RequestURI())
 }
 
-// uri: /feed_entry/full_text.json/{entry_uri}/{uri}
+// uri: /feed_entry/full_text.json/{entry_uri}
 func webapi_feedentry_fulltext(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("uri")
+	uri := r.URL.Query().Get("uri")
 	entry_uri := r.URL.Query().Get("entry_uri")
-	ff, err := feedentry_fulltext(url, entry_uri)
+	log.Println("feed-entry-fullt", entry_uri)
+	ff, err := feedentry_fulltext(uri, entry_uri)
 	if err != nil {
 		webapi_write_error(w, err)
 	} else {
 		webapi_write_as_json(w, ff)
 	}
-	log.Println(r.URL.RequestURI())
 }
 
 // uri: /feed_entry/media.json/{entry_id}/{url}/{media_type:[0-9]+}
-
 func webapi_feedentry_media(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("uri")
+	uri := r.URL.Query().Get("uri")
 	id := r.URL.Query().Get("entry_id")
+	log.Println("feedentry-media", uri, id)
 	media_type, err := strconv.ParseUint(r.URL.Query().Get("media_type"), 0, 0)
 	if err != nil {
 		media_type = uint64(feed.Feed_media_type_unknown)
 	}
-	v, err := feedentry_media(url, id, uint(media_type))
+	v, err := feedentry_media(uri, id, uint(media_type))
 	if err != nil {
 		webapi_write_error(w, err)
 	} else {
 		webapi_write_as_json(w, v)
 	}
-	log.Println(r.URL.RequestURI())
 }
 
 // uri: /feed_entry/drop.json/{entry_id}
-
 // id is mongo's _id
 func webapi_feedentry_drop(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("entry_id")
+	log.Println("feedentry-drop", id)
 	err := feedentry_drop(id)
 	webapi_write_error(w, err)
-	log.Println(r.URL.RequestURI())
 }
 
 // uri: /feed_category/create.json/{name}
 func webapi_feedcategory_create(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
+	log.Println("feed-cat", name)
 	v, err := feedcategory_create(name)
 	if err != nil {
 		webapi_write_error(w, err)
 	} else {
 		webapi_write_as_json(w, v)
 	}
-	log.Println(r.URL.RequestURI())
 }
 
 // uri: /feed_catetory/drop.json/{name}
 func webapi_feedcategory_drop(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("name")
+	log.Println("feed-cat", name)
 	err := feedcategory_drop(name)
 	webapi_write_error(w, err)
-	log.Println(r.URL.RequestURI())
 }
 
 // uri: /tick.json
@@ -249,10 +261,10 @@ func webapi_feedsource_all(w http.ResponseWriter, r *http.Request) {
 
 // uri: /feed_source/unsubscribe.json/{uri}
 func webapi_feedsource_unsubscribe(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("uri")
-	err := feedsource_unsubscribe(url)
+	uri := r.URL.Query().Get("uri")
+	log.Println("feed-unsub", uri)
+	err := feedsource_unsubscribe(uri)
 	webapi_write_error(w, err)
-	log.Println(r.URL.RequestURI())
 }
 
 // uri: /meta/cleanup.json
@@ -264,21 +276,21 @@ func webapi_meta_cleanup(w http.ResponseWriter, r *http.Request) {
 
 // uri: /image/description.json?uri=
 func webapi_image_description(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("uri")
-	v, err := image_description(url)
+	uri := r.URL.Query().Get("uri")
+	log.Println("img-desc", uri)
+	v, err := image_description(uri)
 	if err != nil {
 		webapi_write_error(w, err)
 	} else {
 		webapi_write_as_json(w, v)
 	}
-	log.Println(r.URL.RequestURI())
 }
 
 // uri: /image/dimension.json?uri=
 func webapi_image_dimension(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL.RequestURI())
-	url := r.URL.Query().Get("uri")
-	v, err := image_dimension(url)
+	uri := r.URL.Query().Get("uri")
+	log.Println("img-dim", uri)
+	v, err := image_dimension(uri)
 	if err != nil {
 		webapi_write_error(w, err)
 	} else {
@@ -286,8 +298,9 @@ func webapi_image_dimension(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func webapi_image_videothumbnail(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("uri")
-	switch vt, err := image_videothumbnail(url); err {
+	uri := r.URL.Query().Get("uri")
+	log.Println("img-vthumb", uri)
+	switch vt, err := image_videothumbnail(uri); err {
 	case nil:
 		webapi_image_entity(vt.Image, w, r)
 	default:
@@ -296,7 +309,7 @@ func webapi_image_videothumbnail(w http.ResponseWriter, r *http.Request) {
 }
 
 func webapi_image_entity(uri string, w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL.RequestURI())
+	log.Println("img-entry", uri)
 	switch cache, err := image_description(uri); err {
 	case nil:
 		w.Header().Set("content-type", cache.Mime)
@@ -318,8 +331,9 @@ func webapi_image_thumbnail(w http.ResponseWriter, r *http.Request) {
 
 // /api/image/origin.json?uri=
 func webapi_image_origin(w http.ResponseWriter, r *http.Request) {
-	url := r.URL.Query().Get("uri")
-	switch cache, err := image_description(url); err {
+	uri := r.URL.Query().Get("uri")
+	log.Println("img-origin", uri)
+	switch cache, err := image_description(uri); err {
 	case nil:
 		w.Header().Set("content-type", cache.Mime)
 		w.Header().Set("x-image-meta-property-height", strconv.Itoa(cache.Height))
@@ -329,7 +343,6 @@ func webapi_image_origin(w http.ResponseWriter, r *http.Request) {
 	default:
 		webapi_write_error_code(w, err, cache.Code)
 	}
-	log.Println(r.URL.RequestURI())
 }
 
 func webapi_link_origin(w http.ResponseWriter, r *http.Request) {
