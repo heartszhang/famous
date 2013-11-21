02463e22fdf9d02530970646cb87547b60281aec
hearts <hearts.zhang@gmail.com>
2013-11-21 21:12:59 +0800

    - 捕获httpget过程出现的webexception和taskcancelexception
    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- famousfront/utils/HttpClientUtils.cs
+++ famousfront/utils/HttpClientUtils.cs
@@ -7,43 +7,55 @@ using System.Net.Http.Headers;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
 
 namespace famousfront.utils
 {
-    class ResultWithError<DataType>
+  class ResultWithError<DataType>
+  {
+    internal DataType data = default(DataType);
+    internal int code = -1;
+    internal string reason;
+  }
+  class HttpClientUtils
+  {
+    internal static async Task<ResultWithError<DataType>> Get<DataType>(string url)
     {
-        internal DataType data = default(DataType);
-        internal int code = -1;
-        internal string reason;
-    }
-    class HttpClientUtils
-    {
-        internal static async Task<ResultWithError<DataType>> Get<DataType>(string url)
+      using (var client = new HttpClient())
+      {
+        try
         {
-            try
-            {
-                using (var client = new HttpClient())
-                {
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    var resp = await client.GetAsync(url);
-                    var sc = resp.StatusCode;
-                    if (sc != System.Net.HttpStatusCode.OK)
-                    {
-                        var r = await resp.Content.ReadAsAsync<BackendError>();
-                        return new ResultWithError<DataType> { code = r.code, reason = r.reason };
-                    }
-                    var r2 = await resp.Content.ReadAsAsync<DataType>();
-                    return new ResultWithError<DataType> { data = r2, code = 0, reason = resp.ReasonPhrase };
-                }
-            }
-            catch (Newtonsoft.Json.JsonException e)
-            {
-                return new ResultWithError<DataType> { code = -1, reason = e.Message };
-            }
-            catch (HttpRequestException e)
-            {
-                return new ResultWithError<DataType> { code = -2, reason = e.Message };
-            }            
+          client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+          var resp = await client.GetAsync(url);
+          var sc = resp.StatusCode;
+          if (sc != System.Net.HttpStatusCode.OK)
+          {
+            var r = await resp.Content.ReadAsAsync<BackendError>();
+            return new ResultWithError<DataType> { code = r.code, reason = r.reason };
+          }
+          var r2 = await resp.Content.ReadAsAsync<DataType>();
+          return new ResultWithError<DataType> { data = r2, code = 0, reason = resp.ReasonPhrase };
         }
+        catch (Newtonsoft.Json.JsonException e)
+        {
+          return new ResultWithError<DataType> { code = -1, reason = e.Message };
+        }
+        catch (HttpRequestException e)
+        {
+          return new ResultWithError<DataType> { code = -2, reason = e.Message };
+        }
+        catch (System.Net.WebException e)
+        {
+          return new ResultWithError<DataType> { code = -3, reason = e.Message };
+        }
+        catch (System.Threading.Tasks.TaskCanceledException e)
+        {
+          return new ResultWithError<DataType> { code = -4, reason = e.Message };
+        }
+      }
+
     }
+  }
+
 }
