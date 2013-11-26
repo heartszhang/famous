84f3a779f5ade78d5d2cfd42b735521cbc09c464
hearts <hearts.zhang@gmail.com>
2013-11-26 19:15:41 +0800

    - clean codes
    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- src/github.com/heartszhang/feedfeed/constants.go
+++ src/github.com/heartszhang/feedfeed/constants.go
@@ -86,11 +86,11 @@ type FeedLink struct {
 	Links        int    `json:"links" bson:"links"`                                     // links after cleaned
 	Density      int    `json:"density" bson:"density"`                                 // density of original doc
 	Length       int64  `json:"length" bson:"length"`
-//	Readable     bool   `json:"readable" bson:"readable"` // cleaned doc has perfect content
+	//	Readable     bool   `json:"readable" bson:"readable"` // cleaned doc has perfect content
 
-//	Images []FeedMedia `json:"images,omitempty" bson:"images,omitempty"`
-//	Videos []FeedMedia `json:"videos,omitempty" bson:"videos,omitempty"`
-//	Audios []FeedMedia `json:"audios,omitempty" bson:"audios,omitempty"`
+	//	Images []FeedMedia `json:"images,omitempty" bson:"images,omitempty"`
+	//	Videos []FeedMedia `json:"videos,omitempty" bson:"videos,omitempty"`
+	//	Audios []FeedMedia `json:"audios,omitempty" bson:"audios,omitempty"`
 }
 
 type FeedMedia struct {
@@ -150,6 +150,7 @@ type FeedEntry struct {
 	Density    uint        `json:"density" bson:"density"` // percent
 	Status     uint64      `json:"status" bson:"status"`
 	Categories []string    `json:"categories,omitempty" bson:"category,omitempty"`
+	Readed     bool        `json:"readed" bson:"readed"`
 }
 
 const (
@@ -170,7 +171,6 @@ type FeedSource struct {
 	Description string   `json:"description,omitempty" bson:"description,omitempty"`
 	Logo        string   `json:"logo,omitempty" bson:"logo,omitempty"`
 	Disabled    bool     `json:"disabled" bson:"disabled"` //auto refresh enabled
-	UnreadCount int      `json:"unreaded" bson:"unreaded"`
 	LastTouch   int64    `json:"last_touch" bson:"last_touch"`
 	NextTouch   int64    `json:"next_touch" bson:"next_touch"`
 	LastUpdate  int64    `json:"last_update" bson:"last_update"`
