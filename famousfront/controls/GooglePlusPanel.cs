37cbd202ccb9747993a7657b49ab621d7e9492d1
hearts <hearts.zhang@gmail.com>
2013-12-05 09:04:04 +0800

    - add ExpandableStackPanel
    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- famousfront/controls/GooglePlusPanel.cs
+++ famousfront/controls/GooglePlusPanel.cs
@@ -136,4 +136,51 @@ namespace famousfront.controls
       return idx;
     }
   }
+
+  public class ExpandableStackPanel : StackPanel
+  {
+    public static readonly DependencyProperty CanExpandProperty =
+        DependencyProperty.Register("CanExpand", typeof(bool),typeof(ExpandableStackPanel));
+
+    public static readonly DependencyProperty IsExpandedProperty =
+        DependencyProperty.Register("IsExpanded", typeof(bool),
+        typeof(ExpandableStackPanel), new FrameworkPropertyMetadata(false, 
+          FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsArrange));
+
+    public static readonly DependencyProperty MiniHeightProperty =
+        DependencyProperty.Register("MiniHeight", typeof(double),
+        typeof(ExpandableStackPanel), new FrameworkPropertyMetadata(412d, 
+          FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+    public bool CanExpand
+    {
+      get { return (bool)GetValue(CanExpandProperty); }
+      set { SetValue(CanExpandProperty, value); }
+    }
+
+    public bool IsExpanded
+    {
+      get { return (bool)GetValue(IsExpandedProperty); }
+      set { SetValue(IsExpandedProperty, value); }
+    }
+    public double MiniHeight
+    {
+      get { return (double)GetValue(MiniHeightProperty); }
+      set { SetValue(MiniHeightProperty, value); }
+    }
+    protected override Size MeasureOverride(Size constraint)
+    {
+      var sz = base.MeasureOverride(constraint);
+      CanExpand = sz.Height.gt(MiniHeight);
+      if (!CanExpand)
+      {
+        return sz;
+      }
+      if (!IsExpanded)
+      {
+        sz.Height = MiniHeight;
+      }
+      return sz;
+    }
+  }
 }
