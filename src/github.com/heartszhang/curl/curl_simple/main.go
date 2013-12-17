ef9833bbf990e81d61637cddb90ba0a170f59e60
hearts <hearts.zhang@gmail.com>
2013-12-17 11:27:55 +0800

    Signed-off-by: hearts <hearts.zhang@gmail.com>

--- src/github.com/heartszhang/curl/curl_simple/main.go
+++ src/github.com/heartszhang/curl/curl_simple/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"flag"
-	//	"fmt"
+	"fmt"
 	"github.com/heartszhang/curl"
 )
 
 var (
-	uri = flag.String("uri", "http://www.baidu.com/", "testing url")
+	uri = flag.String("uri", "http://www.voachinese.com/img/assignedIcons/icon-blank.gif", "testing url")
 )
 
 func main() {
 	flag.Parse()
 	curler := curl.NewCurl("")
-	curler.Get(*uri)
+	cache, err := curler.Get(*uri)
+	fmt.Println(cache, err)
+	fp, m, w, h, err := curl.NewThumbnail(cache.Local, "", 400, 0)
+	fmt.Println(fp, m, w, h, err)
 }
